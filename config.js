a26d00f6f521054322d09b7808bac12041e81865
DuncanReyneke <44454233+DuncanReyneke@users.noreply.github.com>
2023-05-12 09:54:03 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  consumer_key: "p5WX4C6Wz4wQaak28R5l1nzkS",
+  consumer_secret: "cilKpF2AaguHtfMHXTzt9zshgsS8cL5n7D8rUU2IknzwDATrsT",
+  access_token_key: "46048696-wPdsYJ4HrywaPuHp717Sh6PSCwWo6iAqlgdWGXmy6",
+  access_token_secret: "IobShggRy2wzmfea08XJ7tUv22n9ecWOjEoQ0ZD3N9zQ3",
+};
